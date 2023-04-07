01e63185c27466eb39e4a3c3b83830c84f0d3da3
Md Nishath Khandakar <wpprodeveloper@gmail.com>
2023-04-07 21:09:24 +0600

    vite+tailwind project setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# What Includs?
+
+- Vite
+- react-router-dom
+- react-icons
+- tailwindcss
+
+## To run this project
+
+```
+git clone https://github.com/mdnishath/vite-tailwind.git
+```
+
+```
+npm i
+```
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 className="px-10 text-4xl font-bold text-pink-500">App</h1>;
+}
+
+export default App;
